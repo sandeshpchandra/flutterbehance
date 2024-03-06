11c72ec56fb87a3926719be072bd9270bf75d2d0
sandesh <sandeshpchandran@gmail.com>
2024-03-06 18:47:57 +0530

    second push

--- lib/secondpage.dart
+++ lib/secondpage.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class SecondPage extends StatefulWidget {
-  const SecondPage({super.key});
+  const SecondPage({Key? key}) : super(key: key);
 
   @override
   State<SecondPage> createState() => _SecondPageState();
@@ -108,51 +108,57 @@ class _SecondPageState extends State<SecondPage> {
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 70,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 70,
+            ),
+            TextField(
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+                suffixIcon: Icon(
+                  Icons.menu_sharp,
+                  color: Colors.white,
+                ),
+                focusColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                hintText: "enter element to search",
+                hintStyle: TextStyle(color: Colors.white),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide(color: Colors.white, width: 10),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(30),
+                ),
               ),
-              TextField(
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                    prefixIcon: Icon(
-                      Icons.search,
-                      color: Colors.white,
-                    ),
-                    suffixIcon: Icon(
-                      Icons.menu_sharp,
-                      color: Colors.white,
-                    ),
-                    focusColor: Colors.white,
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    hintText: "enter elemnt to search",
-                    hintStyle: TextStyle(color: Colors.white),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(color: Colors.white, width: 10)),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                        borderRadius: BorderRadius.circular(30))),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Top search",
+              style: TextStyle(
+                color: Color(0xFFF5F378),
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(
-                height: 30,
-              ),
-              Text(
-                "Top search",
-                style: TextStyle(
-                    color: Color(0xFFF5F378),
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Wrap(spacing: 12, children: [
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Wrap(
+              spacing: 12,
+              children: [
                 OutlinedButton(
                   onPressed: null,
                   style: ButtonStyle(
@@ -208,7 +214,7 @@ class _SecondPageState extends State<SecondPage> {
                     ),
                   ),
                   child: Text(
-                    "Buisness",
+                    "Business",
                     style: TextStyle(
                       fontSize: 15,
                       color: Color(0xffffffff),
@@ -258,168 +264,177 @@ class _SecondPageState extends State<SecondPage> {
                     textAlign: TextAlign.center,
                   ),
                 ),
-              ]),
-              SizedBox(
-                height: 30,
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Categories",
+              style: TextStyle(
+                color: Color(0xFFF5F378),
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
               ),
-              Text(
-                "Categories",
-                style: TextStyle(
-                    color: Color(0xFFF5F378),
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: Row(
-                  children: [
-                    Text(
-                      "Graphic design",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      width: 234,
-                    ),
-                    Icon(
-                      Icons.arrow_right_sharp,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Row(
+                children: [
+                  Text(
+                    "Graphic design",
+                    style: TextStyle(
                       color: Colors.white,
-                      size: 25,
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: Row(
-                  children: [
-                    Text(
-                      "Marketing",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
-                    SizedBox(
-                      width: 273,
-                    ),
-                    Icon(
-                      Icons.arrow_right_sharp,
+                  ),
+                  SizedBox(
+                    width: 234,
+                  ),
+                  Icon(
+                    Icons.arrow_right_sharp,
+                    color: Colors.white,
+                    size: 25,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Row(
+                children: [
+                  Text(
+                    "Marketing",
+                    style: TextStyle(
                       color: Colors.white,
-                      size: 25,
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: Row(
-                  children: [
-                    Text(
-                      "Web design",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
-                    SizedBox(
-                      width: 261,
-                    ),
-                    Icon(
-                      Icons.arrow_right_sharp,
+                  ),
+                  SizedBox(
+                    width: 273,
+                  ),
+                  Icon(
+                    Icons.arrow_right_sharp,
+                    color: Colors.white,
+                    size: 25,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Row(
+                children: [
+                  Text(
+                    "Web design",
+                    style: TextStyle(
                       color: Colors.white,
-                      size: 25,
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: Row(
-                  children: [
-                    Text(
-                      "Business ",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
-                    SizedBox(
-                      width: 277,
-                    ),
-                    Icon(
-                      Icons.arrow_right_sharp,
+                  ),
+                  SizedBox(
+                    width: 261,
+                  ),
+                  Icon(
+                    Icons.arrow_right_sharp,
+                    color: Colors.white,
+                    size: 25,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Row(
+                children: [
+                  Text(
+                    "Business ",
+                    style: TextStyle(
                       color: Colors.white,
-                      size: 25,
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: Row(
-                  children: [
-                    Text(
-                      "Illustration",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
-                    SizedBox(
-                      width: 268,
-                    ),
-                    Icon(
-                      Icons.arrow_right_sharp,
+                  ),
+                  SizedBox(
+                    width: 277,
+                  ),
+                  Icon(
+                    Icons.arrow_right_sharp,
+                    color: Colors.white,
+                    size: 25,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Row(
+                children: [
+                  Text(
+                    "Illustration",
+                    style: TextStyle(
                       color: Colors.white,
-                      size: 25,
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: Row(
-                  children: [
-                    Text(
-                      "See all courses",
-                      style: TextStyle(
-                          color: Color(0xFFF5F378),
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Icon(
-                      Icons.arrow_right_sharp,
+                  ),
+                  SizedBox(
+                    width: 268,
+                  ),
+                  Icon(
+                    Icons.arrow_right_sharp,
+                    color: Colors.white,
+                    size: 25,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Row(
+                children: [
+                  Text(
+                    "See all courses",
+                    style: TextStyle(
                       color: Color(0xFFF5F378),
-                      size: 25,
-                    )
-                  ],
-                ),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Icon(
+                    Icons.arrow_right_sharp,
+                    color: Color(0xFFF5F378),
+                    size: 25,
+                  )
+                ],
               ),
-            ]),
+            ),
+          ],
+        ),
       ),
     );
   }
